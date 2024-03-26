07d9c7b5b37807f1b42e5e133795fc1f5289ee1b
PijusVose <pijus.vose@gmail.com>
2024-03-26 15:23:31 +0200

    finished details; added episodes dropdown; added pagination to episodes

--- src/app/characters/details/details.component.ts
+++ src/app/characters/details/details.component.ts
@@ -3,11 +3,13 @@ import { Character } from '../../types';
 import { ActivatedRoute } from '@angular/router';
 import { CharacterService } from '../../services/character.service';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator'
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatExpansionModule, MatPaginatorModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
@@ -17,6 +19,11 @@ export class DetailsComponent implements OnInit {
 
   id?: string;
   character?: Character;
+  pageEvent?: PageEvent;
+
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25];
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') || '';
@@ -28,5 +35,15 @@ export class DetailsComponent implements OnInit {
 
   getCharacterImageUrl(): string {
     return this.character?.image!;
+  }
+
+  getPaginatedEpisodes() {
+    return this.character?.episode.slice(this.pageIndex * this.pageSize, (this.pageIndex + 1) * this.pageSize);
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
   }
 }
